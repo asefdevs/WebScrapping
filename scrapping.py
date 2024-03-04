04c28f8f9a8dd70447bd72c84106b4a4711df73c
Asef Guluzade <asefdevs@gmail.com>
2024-03-05 00:01:52 +0300

    last update

--- scrapping.py
+++ scrapping.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
+import random
 
 chrome_driver_path = "/usr/local/bin/chromedriver"
 service = webdriver.chrome.service.Service(chrome_driver_path)
@@ -10,6 +11,7 @@ driver  = webdriver.Chrome(service=service)
 def image_scrapping(url, find_method, find_photo, find_button, location, filename, loop=1):
     driver.get(url)
     for i in range(loop):
+            random_name = random.random()
             time.sleep(5)
             if find_method == "class":
                 image = driver.find_element(By.CLASS_NAME, find_photo)
@@ -19,23 +21,23 @@ def image_scrapping(url, find_method, find_photo, find_button, location, filenam
                 refresh_button = driver.find_element(By.ID, find_button)
 
 
-            image.screenshot(f"images/{location}/{filename}{i}.png")
+            image.screenshot(f"images/{location}/{filename}{i}{random_name}.png")
             refresh_button.click()
     driver.quit()
 ankara_link = "https://online.ankarasigorta.com.tr/"
 ankara_find_method = "class"
 ankara_find_photo = "captcha-image"
-ankara_find_button = "captcha-refresh"
-ankara_location = "ankarasigorta"
-ankara_filename = "ankarasigorta"
+ankara_find_button = "btn-refresh"
+ankara_location = "ankara"
+ankara_filename = "ankara"
 
 
 somposigorta_link = "https://ejento.somposigorta.com.tr/"
 somposigorta_find_method = "id"
 somposigorta_find_photo = "capthcaLogin_IMG"
 sompos_find_button = "capthcaLogin_RTS"
-somposigorta_location = "somposigorta"
-somposigorta_filename = "somposigorta"
+somposigorta_location = "sompos"
+somposigorta_filename = "sompos"
 
 axatek_link = "https://axatek.axasigorta.com.tr/login"
 axatek_find_method = "class"
@@ -44,4 +46,4 @@ axatek_find_button = "v-btn__content"
 axatek_location = "axatek"
 axatek_filename = "axatek"
 
-image_scrapping("https://online.ankarasigorta.com.tr/", "class", "captcha-image", "btn-refresh", "ankara", "ankarasigorta", 10)
+image_scrapping(ankara_link, ankara_find_method, ankara_find_photo, ankara_find_button, ankara_location, ankara_filename, 1)
